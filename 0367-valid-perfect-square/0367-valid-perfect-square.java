e77e66514b939df79b622320158e8be29c7247b6
VIJAYVJX <vijayajay463@gmail.com>
2025-04-06 12:54:36 +0530

    Time: 0 ms (100%), Space: 40.2 MB (93.24%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+       int a= (int)Math.sqrt(num);
+       int b=a*a;
+       if(b==num) return true;
+       else return false;
+    }
+}
