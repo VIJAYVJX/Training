6b5caba98b188b1b7dd83e66f8c8510ce7704420
VIJAYVJX <vijayajay463@gmail.com>
2025-04-06 10:23:00 +0530

    Time: 145 ms (5.62%), Space: 44 MB (75.95%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if(x<0){
-            return false;
-            }
-
+        return false;
+        }
         int temp=x;
-        int rev=0,rem;
+        int rev=0 ,rem;
         while(temp!=0){
-            rem=temp%10;
-            rev=(rev*10)+rem;
+            rem =temp%10;
+            rev =(rev*10)+rem;
             temp=temp/10;
         }
-        return(rev==x);
-        }
+        return(rev == x);
     }
+}
